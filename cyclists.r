f5825b05dac22d7700f45bb42ac1a2bf1d6fe3ae
Thomas Levine <occurrence@thomaslevine.com>
2013-04-06 18:14:41 -0400

    aoeu

--- cyclists.r
+++ cyclists.r
@@ -1,2 +1,14 @@
 #!/usr/bin/env Rscript
-cyclists <- read.csv('cyclists.csv')
+library(plyr)
+library(reshape2)
+# library(ggplot2)
+
+cyclists <- read.csv('cyclists.csv', stringsAsFactors = F)
+rownames(cyclists) <- strptime(paste(cyclists$Month, ' 1, ', cyclists$Year, sep = ''), format='%B %d, %Y')
+cyclists$Date <- rownames(cyclists)
+cyclists$Month <- NULL
+cyclists$Year <- NULL
+cyclists$Grand.Total <- NULL
+
+# cyclists <- melt(cyclists, 'Date', variable.name = 'Bridge', value.name = 'Cyclists')
+# cyclists$Date <- as.POSIXct(cyclists$Date)
